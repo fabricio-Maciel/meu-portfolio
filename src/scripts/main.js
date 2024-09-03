132288ff6db81ac86cdec6903d7abb23df8d34b4
Fabricio Maciel <fabriciomaciel.dev@gmail.com>
2024-09-03 16:14:56 -0300

    Criacao do menu hamburguer

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,15 @@
 const btnMenu = document.getElementById("btn-menu");
 
-function toggleMenu() {
+function toggleMenu(event) {
   const nav = document.getElementById("menu");
   nav.classList.toggle("active");
+  const active = nav.classList.contains("active");
+  event.currentTarget.setAttribute("aria-expanded", active);
+  if (active) {
+    event.currentTarget.setAttribute("aria-label", "Fechar menu");
+  } else {
+    event.currentTarget.setAttribute("aria-label", "Abrir Menu");
+  }
 }
 
 btnMenu.addEventListener("click", toggleMenu);
